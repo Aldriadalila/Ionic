aa758b6180b6f7bbe7cd92b3c70b7aece74c957c
Aldria Dalila Domingues de Oliveira <aldriaoliveira3@gmail.com>
2018-11-05 00:36:27 -0200

    Ionic 05

---
+++ pages/crud/crud.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import { TabsPage } from '../tabs/tabs';
+import { ContactPage } from '../contact/contact';
+import { FilmePage } from '../filme/filme';
+
+/**
+ * ClaudemirCardoso RA: 816155452
+ *
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-crud',
+  templateUrl: 'crud.html',
+  
+})
+export class CrudPage {
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CrudPage');
+  }
+
+  goToTabsPage (){
+    this.navCtrl.push(ContactPage);
+  }
+
+  /*goToInserido (){
+    this.navCtrl.push(CrudPage);
+  }*/
+  
+  goToTabsFilme (){
+    this.navCtrl.push(FilmePage);
+  }
+
+}
---
+++ pages/filme/filme.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-filme',
+  templateUrl: 'filme.html',
+})
+export class FilmePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FilmePage');
+  }
+
+}
